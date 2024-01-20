9cc60539b5b6bcfbad59d1a7b2b4019f092745ce
Sameer Alam <alamsameer738@gmail.com>
2024-01-20 12:50:49 +0530

    updated code folder v2

--- api/index.js
+++ api/index.js
@@ -1,13 +1,29 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { v4 } from 'uuid';
 dotenv.config(); // Include dotenv package to load environment variables
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+// app.get('/', (req, res) => {
+//     res.send('Hello, World!');
+// });
+
+
+
+app.get('/api', (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
 });
+
+app.get('/api/item/:slug', (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
