1544b73e4517fb1d96b1617d958500aad5744f2d
Sameer Alam <alamsameer738@gmail.com>
2024-01-20 12:00:02 +0530

    rewrriten vercel.json

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 
+require('dotenv').config(); // Include dotenv package to load environment variables
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
